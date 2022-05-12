bf2e19af268d13c613f1a636ffa423720f2cce64
VINICIUS YUJI NISHIOKA <87093564+yujinishioka@users.noreply.github.com>
2022-05-11 21:01:40 -0300

    adicionando aula de heranca

---
+++ eclipse-workspace/DDD/src/aula5/Teste.java
@@ -0,0 +1,13 @@
+package aula5;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Honda carro1 = new Honda("2.0 Flex", "Honda Fit", 60000);
+		System.out.println("Primeiro carro");
+		System.out.println("Modelo: " + carro1.getModelo());
+		System.out.println("Cor: " + carro1.getCor());
+		System.out.println("Motor: " + carro1.getMotor());
+		System.out.println("Preco: R$" + carro1.getPreco());
+	}
+}
